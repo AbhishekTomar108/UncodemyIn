44ccbd59d56c0cfb0fb6a48b0bb63cded38884d6
AbhishekTomar108 <abhishektomar9211@gmail.com>
2023-06-19 06:23:02 -0700

    about us added google map added pop up form database connection

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ function addDropdownListener(index){
   dropdownCourse[index].addEventListener("click" , function () {
     // document.querySelector('.dropdown').style.display = 'block'
 
-    console.log('form corse name =',   document.querySelector('.dropdown').style.display)
+
     let activeElement = document.getElementById("active-dropdown-course");
     var activeCourses = document.getElementById("active-course");
     if (activeElement) {
@@ -26,7 +26,7 @@ function addDropdownListener(index){
 
     var addactiveCourses = document.getElementsByClassName('course-image-text')[index];
     addactiveCourses.setAttribute("id", "active-course");
-    console.log('index ',index,dropdownCourseElement )
+
   })
 }
 
@@ -53,7 +53,7 @@ function addEventListenerToCourse(index) {
 
     var addactiveElement = document.getElementsByClassName('course-card-thumb')[index];
     var courseElement = document.getElementsByClassName('course-name')[index];
-    console.log('add active', index, addactiveElement);
+   
     addactiveElement.setAttribute("id", "active");
     const container = addactiveElement.parentElement;
     container.scrollTop = addactiveElement.offsetTop;
@@ -88,18 +88,15 @@ const certificateRight = ()=>{
 
   const totalCertificateWidthToMove = (totalCertificateLenght-1)*certificateWidth;
   const currentCertificateMove = Math.abs(parseFloat(certificateLeftValue));
-  console.log('left value = ',certificateLeftValue,parseInt(certificateLeftValue),certificateWidth)
+  
   // const currentCertificateMove = parseInt(certificateLeftValue);
-  console.log('total width ',totalCertificateWidthToMove)
-  console.log(currentCertificateMove)
+ 
 
   if((currentCertificateMove)>=totalCertificateWidthToMove-1){
-    console.log('if =',currentCertificateMove,totalCertificateWidthToMove)
     certificate.style.left = `${(0)}px`
   }
 
   else{
-    console.log('else =',currentCertificateMove,totalCertificateWidthToMove)
     certificate.style.left = `${(-currentCertificateMove-certificateWidth)}px`
   }
 
@@ -142,8 +139,7 @@ const movealumniright  =()=>{
   // clearInterval(alumniInterval)
 
   const totalAlumniLenght  = document.getElementsByClassName('alumni-image-thumb').length;
-  // const MovedImage = document.querySelector('.alumni-image-detail-section').style.left;
-  // console.log('move image ',MovedImage)
+
 
   const alumni = document.querySelector('.alumni-image-detail-section');
   const computedStyle = window.getComputedStyle(alumni);
@@ -173,8 +169,7 @@ const movealumnileft  =()=>{
   // clearInterval(alumniInterval)
 
   const totalAlumniLenght  = document.getElementsByClassName('alumni-image-thumb').length;
-  // const MovedImage = document.querySelector('.alumni-image-detail-section').style.left;
-  // console.log('move image ',MovedImage)
+
 
   const alumni = document.querySelector('.alumni-image-detail-section');
   const computedStyle = window.getComputedStyle(alumni);
@@ -369,7 +364,6 @@ function expandText(index) {
     showHideText.innerText = "show more...";
   }
   
-  console.log('target element outside if-else', targetElement.style.height);
   
   var computedStyle = window.getComputedStyle(targetElement);
   var currentHeight = computedStyle.getPropertyValue('height');
@@ -379,7 +373,6 @@ function expandText(index) {
   
   if (currentHeight === '67px') {
 
-    console.log('target element', targetElement.scrollHeight);
     targetElement.style.height = `${targetElement.scrollHeight}px`;
     
     // targetElement.style.height = "auto";
@@ -401,20 +394,24 @@ function expandText(index) {
   }
   
   
+}
 
 
+let popUpFormValue ={
+  name:"",
+  mobile:"",
+  email:"",
+  course:"",
+  date:"",
+  fromTime:"",
+  toTime:""
+}
 
-//   if (textElement.length > maxLength) {
-//     console.log('if substr', index, textElement.length,textElement)
-//     // textElement = textElement.substr(0,maxLength) + '...';
-//     textElement = textElement.substr(0,maxLength) + '...';
-// }
-// else{
-//   console.log('if substr', index)
-//   console.log('textelement is running')
-//   textElement = textElement.substr(0,textElement.length)
-// }
-  // textElement.classList.toggle('expanded');
+
+function setPopUpFormValue(name, value){
+ 
+  popUpFormValue[name] = value
+  console.log('pop up value =',popUpFormValue)
 }
 
 
@@ -443,7 +440,6 @@ function hidePopUpForm(){
 
 
 function showForm(){
-  console.log('show form running')
 
   const formConatiner = document.querySelector('.form-parent')
   document.querySelector('.form-parent').style.display="block"
@@ -466,28 +462,10 @@ function hideForm(){
 // --------- Dropdown show function ------------
 
 
-// document.getElementById('course-text').addEventListener('click',()=>{
-
-//   console.log('show drop down')
-//   let dropDown = document.querySelector('.dropdown');
-//   var computedStyle = window.getComputedStyle(dropDown);
-//   if(computedStyle.display==="none"){
-//     dropDown.style.display="flex"
-//     document.getElementById('dropdown-arrow').style.color = "#ff5421"
-//     document.getElementById('course-text').style.color = "#ff5421"
-//   }
-
-//   else{
-//     dropDown.style.display="none"
-//     document.getElementById('dropdown-arrow').style.color = "black"
-//     document.getElementById('course-text').style.color = "black"
-//   }
-// })
 
 
 function showDropDown(){
 
-  console.log('show drop down')
   let dropDown = document.querySelector('.dropdown');
   var computedStyle = window.getComputedStyle(dropDown);
   if(computedStyle.display==="none"){
@@ -547,10 +525,9 @@ function showVideo(url){
 }
 
 function hideVideo(){
-  console.log('hiding')
   document.getElementById('videoContainer').style.display="none"
   
-  // document.getElementById('videoContainer').style.top="0"
+ 
   document.body.style.overflow='auto'
   document.getElementById('overlay').style.display="none"
   var myFrame = document.querySelector("iframe");
@@ -574,22 +551,15 @@ let formValue = {
 
 function setFormValue(name,value){
 
- 
-
-
-  console.log('form running ',name,value)
-
-  // formValue = {...formValue}
   formValue[name]=value;
 
 
-  console.log('form value =',formValue)
 
 }
 
 function numberOnly() {
 
-  console.log('number only runnnin')
+
   // Get element by id which passed as parameter within HTML element event
   var element = document.getElementById('mobile-input');
   // This removes any other character but numbers as entered by user
@@ -602,9 +572,11 @@ function numberOnly() {
   }
 }
 
+
+
 async function submitForm(event) {
   event.preventDefault(); 
-  console.log("submitForm is running")
+
 
   let data = new FormData();
   data.append('name', formValue.name);
@@ -616,7 +588,7 @@ async function submitForm(event) {
  
 
   try{
-    const response = await axios.post('http://localhost/formSubmitUncodemyIn.php', data);
+    const response = await axios.post('https://albuminoid-contempt.000webhostapp.com/formSubmitUncodemyIn.php', data);
 
     
     if(response.data===1){
@@ -630,7 +602,7 @@ async function submitForm(event) {
       Swal.fire({
         icon: 'success',
         title: 'Congratulation!',
-        html:`You are one step closer to have a <span style="color:#ff5124">Career Session</span>. with our Experts Our Team will connect you soon with Detail Information`,
+        html:`You are one step closer to have a <span style="color:#ff5124">Career Session</span> with our Experts. Our Team will connect you soon with Detail Information`,
         showConfirmButton: false,
         timer: 8000
       })
@@ -638,13 +610,64 @@ async function submitForm(event) {
     }
 
     else{
-      console.log('error from else =',response.data)
+      
       alert("sorry some error is occured")
     
     }
   }
   catch(error) {
-    console.log('error =',error)
+   
+    alert("sorry server issue occured please try again")
+  };
+
+
+}
+
+
+
+async function submitPopUpForm(event) {
+  event.preventDefault(); 
+
+
+  let data = new FormData();
+  data.append('name', popUpFormValue.name);
+  data.append('email', popUpFormValue.email);
+  data.append('mobile', popUpFormValue.mobile);
+  data.append('course', popUpFormValue.course);
+  data.append('date', popUpFormValue.date);
+  data.append('fromTime', popUpFormValue.fromTime);
+  data.append('toTime', popUpFormValue.toTime);
+ 
+
+  try{
+    const response = await axios.post('http://localhost/DemoDetailForm.php', data);
+
+    
+    if(response.data===1){
+
+      document.querySelector('.pop-up-form-container').style.display="none"
+      document.body.style.overflow='auto'
+      document.getElementById('overlay').style.display="none"
+      
+
+      Swal.fire({
+        icon: 'success',
+        title: 'Congratulation!',
+        html:`You Queries have been booked for the <span style="color:#ff5124">Demo Session</span> with our Experts. Our Team will connect you soon with Detail Information`,
+        showConfirmButton: false,
+        timer: 8000
+      })
+         
+    }
+
+    else{
+      
+      alert("sorry some error is occured")
+    
+    }
+  }
+  catch(error) {
+   
     alert("sorry server issue occured please try again")
   };
 
